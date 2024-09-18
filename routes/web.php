4b24d52b6049a87fc079aaf35623c870d6c30964
leo AZ <lionel.aziangbe@gmail.com>
2024-09-18 12:57:40 +0000

    wip

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,12 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\VehicleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/vehicles", VehicleController::class)->name("vehicles");
-Route::get("/carriers", CarrierController::class)->name("carriers");
-Route::get("/depots", DepotController::class)->name("depots");
-Route::get("/loads", LoadController::class)->name("loads");
-
-Route::get("/dashboard", function () {
-    return view("dashboard");
-})
-    ->middleware(["auth", "verified"])
-    ->name("dashboard");
-
 Route::middleware("auth")->group(function () {
+    Route::get("/vehicles", VehicleController::class)->name("vehicles");
+    Route::get("/", CarrierController::class)->name("carriers");
+    Route::get("/depots", DepotController::class)->name("depots");
+    Route::get("/loads", LoadController::class)->name("loads");
+
     Route::get("/profile", [ProfileController::class, "edit"])->name(
         "profile.edit"
     );
